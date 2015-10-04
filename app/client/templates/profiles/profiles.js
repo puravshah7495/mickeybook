a7a326da59c5818d83b8c4fc9fc59f66eab1656c
Purav Shah <puravshah23@gmail.com>
2015-10-04 00:34:30 -0400

    Begin setting up profile

--- app/client/templates/profiles/profiles.js
+++ app/client/templates/profiles/profiles.js
@@ -9,6 +9,14 @@ Template.Profiles.events({
 /* Profiles: Helpers */
 /*****************************************************************************/
 Template.Profiles.helpers({
+	template: function() {
+		var template = Session.get('selectedTab');
+		if (!template) {
+			return "Timeline";
+		} else {
+			return template;
+		}
+	}
 });
 
 /*****************************************************************************/
@@ -18,6 +26,7 @@ Template.Profiles.created = function () {
 };
 
 Template.Profiles.rendered = function () {
+	$('#tabs').tabs();
 };
 
 Template.Profiles.destroyed = function () {
